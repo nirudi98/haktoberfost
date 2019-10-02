ebd6685ed1b79dd5f083ccc6fc656b6bf7802d56
nirudi98 <nirupink58@gmail.com>
2019-10-02 10:28:48 +0530

    Fixed PHP document

--- a.php
+++ a.php
@@ -1,3 +1,12 @@
-<?php
-echob "Hello";
-?>
+<html>
+<body>
+	
+
+
+	<?php echo "Hello this is my first php page"; ?>
+	<br><br>
+
+	<h2><font color="red"> Welcome &nbsp; <font color="black"> <?php echo "_enter your name_"; ?> </font> </font></h2><br>
+	<h2> <font color="#B40609"> Your email address is: &nbsp;   <font color="black">  <?php echo "_enter your email_"; ?> </font> </font> </h2>
+</body>
+</html>
